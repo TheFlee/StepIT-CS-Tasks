a5c4dd7d3963f113d439abd434a89bf8a62dda32
Firidun Hasanli <firidunhesenli112@gmail.com>
2025-05-12 13:32:05 +0400

    Add project files.

---
+++ 01. Introduction, Datatypes/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1 = 20;
+int num2 = 8;
+
+Console.WriteLine("Toplama (+): " + (num1 + num2));
+Console.WriteLine("Chixma (-): "+ (num1 - num2));
+Console.WriteLine("Vurma (*): " + (num1 * num2));
+Console.WriteLine("Bolme (/): " + (num1 / num2));
+Console.WriteLine("Mod (%): " + (num1 % num2));
+
