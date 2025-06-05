357eeb8b8ae688a77f87f43c121cf816396bd88c
Firidun Hasanli <firidunhesenli112@gmail.com>
2025-06-05 18:25:06 +0400

    HW11

---
+++ 11. Anonymous methods, LINQ/Debtor.cs
@@ -0,0 +1,23 @@
+﻿class Debtor
+{
+    public Debtor(string fullname, DateTime birthDay, string phone, string email, string address, int debt)
+    {
+        FullName = fullname;
+        BirthDay = birthDay;
+        Phone = phone;
+        Email = email;
+        Address = address;
+        Debt = debt;
+    }
+
+    public string FullName { get; set; }
+    public DateTime BirthDay { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public int Debt { get; set; }
+    public override string ToString()
+    {
+        return $"{FullName} {BirthDay.ToShortDateString()} {Phone} {Email} {Address} {Debt}";
+    }
+}
